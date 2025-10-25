3632d4d4440f9297c73bdad233a9b79949b0128e
Jigisha Baliyann <115288853+Jigisha-Baliyann@users.noreply.github.com>
2025-10-25 19:27:34 +0530

    Solve Day-1

---
+++ 01-Arrays/secondLargest.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n = arr.length;
+        Arrays.sort(arr);
+        int max = arr[n-1];
+        for (int i=n-2; i>=0; i--) 
+            if (arr[i] < max)
+                return arr[i];
+        return -1;
+    }
+}
